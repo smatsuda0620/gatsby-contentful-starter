32bea3cb1161fba2a7af64710c4bd23010488e7b
Shuji Matsuda <shuji.mazdawso@gmail.com>
2019-11-09 17:29:57 +0900

    Remove env file

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,8 @@
-require('dotenv').config({
-  path: `.env.${process.env.NODE_ENV}`
-})
+if (process.env.NODE_ENV === 'development') {
+  require('dotenv').config({
+    path: `.env.${process.env.NODE_ENV}`
+  })
+}
 
 const contentfulConfig = {
   spaceId: process.env.CONTENTFUL_SPACE_ID,
